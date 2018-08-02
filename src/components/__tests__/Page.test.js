848591f9140b55df520894b2316f85c1641d8d84
Thomas Brooks <d3vkit@gmail.com>
2018-08-01 18:51:13 -0700

    [7] Allow return of element from Page

--- src/components/__tests__/Page.test.js
+++ src/components/__tests__/Page.test.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { shallow } from 'enzyme'
 import Page from '../Page'
+import FoundElement from '../FoundElement';
 
 describe('Page', () => {
-  const component = React.createElement('div')
+  const component = <div />
 
   it('can be extended', () => {
     class MyPage extends Page { }
 
     expect(() => { new MyPage('shallow', component) }).not.toThrow()
+  })
+
+  describe('#element', () => {
+    it('returns itself as a FoundElement', () => {
+      const myPage = new Page('shallow', component)
+      const wrapped = shallow(component)
+      const expected = new FoundElement(wrapped)
+
+      const result = myPage.element
+
+      expect(result).toEqual(expected)
+    })
   })
 })
