7b0b04d816c8a9fe61fc8b5ae0e01261d2e7e42a
Thomas Brooks <d3vkit@gmail.com>
2018-08-02 17:16:48 -0700

    [14] Add various getter methods to FoundElement

--- src/components/__tests__/FoundElement.test.js
+++ src/components/__tests__/FoundElement.test.js
@@ -2,281 +2,355 @@ import React from 'react'
 import enzyme, { shallow } from 'enzyme'
 import FoundElement from '../FoundElement'
 
-describe('constructor', () => {
-  it('assigns the element to itself', () => {
-    const element = React.createElement('div')
-    const foundElement = new FoundElement(element)
-
-    const result = foundElement.element
-
-    expect(result).toEqual(element)
-  })
-})
-
-describe('#find', () => {
-  it('calls find on the element', () => {
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const spy = jest.spyOn(element, 'find')
-    const foundElement = new FoundElement(element)
-
-    foundElement.find('.my-klass')
-
-    expect(spy).toHaveBeenCalledTimes(1)
-  })
-
-  it('passes the given selector to find', () => {
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const spy = jest.spyOn(element, 'find')
-    const foundElement = new FoundElement(element)
-    const selector = '.my-klass'
-
-    foundElement.find(selector)
-
-    expect(spy).toHaveBeenCalledWith(selector)
-  })
-
-  it('replaces element with the result of the find', () => {
-    const selector = '.my-klass'
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const expected = element.find(selector)
-    const foundElement = new FoundElement(element)
-
-    foundElement.find(selector)
-
-    expect(foundElement.element).toEqual(expected)
-  })
-
-  it('returns itself', () => {
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const foundElement = new FoundElement(element)
-
-    const result = foundElement.find('.my-klass')
-
-    expect(result).toBe(foundElement)
-  })
-})
-
-describe('#filter', () => {
-  it('calls filter on the element', () => {
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const spy = jest.spyOn(element, 'filter')
-    const foundElement = new FoundElement(element)
-
-    foundElement.filter('.my-klass')
-
-    expect(spy).toHaveBeenCalledTimes(1)
-  })
-
-  it('passes the given selector to filter', () => {
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const spy = jest.spyOn(element, 'filter')
-    const foundElement = new FoundElement(element)
-    const selector = '.my-klass'
-
-    foundElement.filter(selector)
-
-    expect(spy).toHaveBeenCalledWith(selector)
-  })
-
-  it('replaces element with the result of the filter', () => {
-    const selector = '.my-klass'
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const expected = element.find(selector)
-    const foundElement = new FoundElement(element)
-
-    foundElement.filter(selector)
-
-    expect(foundElement.element).toEqual(expected)
-  })
-
-  it('returns itself', () => {
-    const element = shallow(<div className="my-klass">My Div</div>)
-    const foundElement = new FoundElement(element)
-
-    const result = foundElement.filter('.my-klass')
-
-    expect(result).toBe(foundElement)
-  })
-})
-
-describe('#length', () => {
-  it('returns the length', () => {
-    const element = shallow(<div>My Div</div>)
-    const foundElement = new FoundElement(element)
-
-    const result = foundElement.length
-
-    expect(result).toBe(1)
-  })
-})
-
-describe('#map', () => {
-  describe('when wrap option is not defined', () => {
-    it('returns array of results wrapped in found elements', () => {
-      const myKlass = <div className="my-klass">My Div</div>
-      const otherKlass = <div className="other-klass">My Div</div>
-      const element = shallow(<div>{myKlass}{otherKlass}</div>)
+describe('FoundElement', () => {
+  describe('constructor', () => {
+    it('assigns the given object to element', () => {
+      const element = <div />
       const foundElement = new FoundElement(element)
-      const expected = new FoundElement(shallow(myKlass))
 
-      const result = foundElement.map(node => node.find('.my-klass'))
+      const result = foundElement.element
 
-      expect(result).toEqual([expected])
+      expect(result).toEqual(element)
     })
   })
 
-  describe('when wrap option is true', () => {
-    it('returns array of results wrapped in found elements', () => {
-      const myKlass = <div className="my-klass">My Div</div>
-      const otherKlass = <div className="other-klass">My Div</div>
-      const element = shallow(<div>{myKlass}{otherKlass}</div>)
-      const foundElement = new FoundElement(element)
-      const expected = new FoundElement(shallow(myKlass))
+  describe('#change', () => {
+    describe('when called on a text input', () => {
+      const wrapper = shallow(<input value="Old Value" type="text" />)
+      const spy = jest.spyOn(wrapper, 'simulate')
 
-      const result = foundElement.map((node) => node.find('.my-klass'), true)
+      it('calls simulate "change" on the element', () => {
+        const foundElement = new FoundElement(wrapper)
 
-      expect(result).toEqual([expected])
+        foundElement.change('New Value')
+
+        expect(spy).toHaveBeenCalledTimes(1)
+      })
+
+      it('merges the given options to simulate', () => {
+        const foundElement = new FoundElement(wrapper)
+        const value = 'New Value'
+        const options = { target: { name: 'superman' } }
+        const expectedOptions = {
+          target: { name: 'superman', value }
+        }
+
+        foundElement.change(value, options)
+
+        expect(spy).toHaveBeenCalledWith('change', expectedOptions)
+      })
+
+      it('does not accept target value given in options', () => {
+        const foundElement = new FoundElement(wrapper)
+        const value = 'New Value'
+        const options = { target: { value: 'Old Value' } }
+        const expectedOptions = {
+          target: { value, }
+        }
+
+        foundElement.change(value, options)
+
+        expect(spy).toHaveBeenCalledWith('change', expectedOptions)
+      })
+    })
+
+    describe('when called on a checkbox', () => {
+      const wrapper = shallow(<input checked="false" type="checkbox" />)
+      const spy = jest.spyOn(wrapper, 'simulate')
+
+      it('calls simulate "change" on the element', () => {
+        const foundElement = new FoundElement(wrapper)
+
+        foundElement.change(true)
+
+        expect(spy).toHaveBeenCalledTimes(1)
+      })
+
+      it('merges the given options to simulate', () => {
+        const foundElement = new FoundElement(wrapper)
+        const value = true
+        const options = { target: { name: 'superman' } }
+        const expectedOptions = {
+          target: { name: 'superman', checked: value }
+        }
+
+        foundElement.change(value, options)
+
+        expect(spy).toHaveBeenCalledWith('change', expectedOptions)
+      })
+
+      it('does not accept target checked given in options', () => {
+        const foundElement = new FoundElement(wrapper)
+        const value = true
+        const options = { target: { checked: false } }
+        const expectedOptions = {
+          target: { checked: value }
+        }
+
+        foundElement.change(value, options)
+
+        expect(spy).toHaveBeenCalledWith('change', expectedOptions)
+      })
     })
   })
 
-  describe('when wrap options is false', () => {
-    it('returns array of results not wrapped in found elements', () => {
-      const myKlass = <div className="my-klass">My Div</div>
-      const otherKlass = <div className="other-klass">My Div</div>
-      const element = shallow(<div>{myKlass}{otherKlass}</div>)
-      const foundElement = new FoundElement(element)
-      const expected = shallow(myKlass)
+  describe('#click', () => {
+    const wrapper = shallow(<div className="my-klass">My Div</div>)
+    const spy = jest.spyOn(wrapper, 'simulate')
 
-      const result = foundElement.map(node => node.find('.my-klass'), false)
+    it('calls simulate "click" on the element', () => {
+      const foundElement = new FoundElement(wrapper)
 
-      expect(result).toEqual([expected])
-    })
-  })
-})
-
-describe('#prop', () => {
-  it('returns the specific prop', () => {
-    const element = shallow(<div foo="bar">My Div</div>)
-    const foundElement = new FoundElement(element)
-
-    const result = foundElement.prop('foo')
-
-    expect(result).toBe('bar')
-  })
-})
-
-describe('#props', () => {
-  it('returns the props', () => {
-    const element = shallow(<div foo="bar">My Div</div>)
-    const foundElement = new FoundElement(element)
-
-    const result = foundElement.props()
-
-    expect(result).toEqual({ children: 'My Div', foo: 'bar' })
-  })
-})
-
-describe('#click', () => {
-  const element = { simulate: jest.fn(() => {}) }
-
-  it('calls simulate "click" on the element', () => {
-    const foundElement = new FoundElement(element)
-
-    foundElement.click('.my-klass')
-
-    expect(element.simulate).toHaveBeenCalledTimes(1)
-  })
-
-  it('passes the given options to simulate', () => {
-    const foundElement = new FoundElement(element)
-    const options = { target: 'new' }
-
-    foundElement.click(options)
-
-    expect(element.simulate).toHaveBeenCalledWith('click', options)
-  })
-})
-
-describe('#change', () => {
-  describe('when called on a text input', () => {
-    const reactElement = React.createElement('input', {
-      value: 'Old Value',
-      type: 'text'
-    })
-    let element = shallow(reactElement)
-    element.simulate = jest.fn(() => { })
-
-    it('calls simulate "change" on the element', () => {
-      const foundElement = new FoundElement(element)
-
-      foundElement.change('New Value')
-
-      expect(element.simulate).toHaveBeenCalledTimes(1)
-    })
-
-    it('merges the given options to simulate', () => {
-      const foundElement = new FoundElement(element)
-      const value = 'New Value'
-      const options = { target: { name: 'superman' } }
-      const expectedOptions = {
-        target: { name: 'superman', value }
-      }
-
-      foundElement.change(value, options)
-
-      expect(element.simulate).toHaveBeenCalledWith('change', expectedOptions)
-    })
-
-    it('does not accept target value given in options', () => {
-      const foundElement = new FoundElement(element)
-      const value = 'New Value'
-      const options = { target: { value: 'Old Value' } }
-      const expectedOptions = {
-        target: { value, }
-      }
-
-      foundElement.change(value, options)
-
-      expect(element.simulate).toHaveBeenCalledWith('change', expectedOptions)
-    })
-  })
-
-  describe('when called on a checkbox', () => {
-    const element = shallow(<input checked="false" type="checkbox" />)
-    const spy = jest.spyOn(element, 'simulate')
-
-    it('calls simulate "change" on the element', () => {
-      const foundElement = new FoundElement(element)
-
-      foundElement.change(true)
+      foundElement.click('.my-klass')
 
       expect(spy).toHaveBeenCalledTimes(1)
     })
 
-    it('merges the given options to simulate', () => {
-      const foundElement = new FoundElement(element)
-      const value = true
-      const options = { target: { name: 'superman' } }
-      const expectedOptions = {
-        target: { name: 'superman', checked: value }
-      }
+    it('passes the given options to simulate', () => {
+      const foundElement = new FoundElement(wrapper)
+      const options = { target: 'new' }
 
-      foundElement.change(value, options)
+      foundElement.click(options)
 
-      expect(spy).toHaveBeenCalledWith('change', expectedOptions)
+      expect(spy).toHaveBeenCalledWith('click', options)
+    })
+  })
+
+  describe('#debug', () => {
+    it('returns enzyme debug', () => {
+      const wrapper = shallow(<div foo="bar">My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+      const expected = wrapper.debug()
+
+      const result = foundElement.debug()
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('#find', () => {
+    it('calls find on the wrapper', () => {
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const spy = jest.spyOn(wrapper, 'find')
+      const foundElement = new FoundElement(wrapper)
+
+      foundElement.find('.my-klass')
+
+      expect(spy).toHaveBeenCalledTimes(1)
     })
 
-    it('does not accept target checked given in options', () => {
-      const foundElement = new FoundElement(element)
-      const value = true
-      const options = { target: { checked: false } }
-      const expectedOptions = {
-        target: { checked: value }
-      }
+    it('passes the given selector to find', () => {
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const spy = jest.spyOn(wrapper, 'find')
+      const foundElement = new FoundElement(wrapper)
+      const selector = '.my-klass'
 
-      foundElement.change(value, options)
+      foundElement.find(selector)
 
-      expect(spy).toHaveBeenCalledWith('change', expectedOptions)
+      expect(spy).toHaveBeenCalledWith(selector)
+    })
+
+    it('replaces element with the result of the find', () => {
+      const selector = '.my-klass'
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const expected = wrapper.find(selector)
+      const foundElement = new FoundElement(wrapper)
+
+      foundElement.find(selector)
+
+      expect(foundElement.element).toEqual(expected)
+    })
+
+    it('returns itself', () => {
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+
+      const result = foundElement.find('.my-klass')
+
+      expect(result).toBe(foundElement)
+    })
+  })
+
+  describe('#filter', () => {
+    it('calls filter on the element', () => {
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const spy = jest.spyOn(wrapper, 'filter')
+      const foundElement = new FoundElement(wrapper)
+
+      foundElement.filter('.my-klass')
+
+      expect(spy).toHaveBeenCalledTimes(1)
+    })
+
+    it('passes the given selector to filter', () => {
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const spy = jest.spyOn(wrapper, 'filter')
+      const foundElement = new FoundElement(wrapper)
+      const selector = '.my-klass'
+
+      foundElement.filter(selector)
+
+      expect(spy).toHaveBeenCalledWith(selector)
+    })
+
+    it('replaces element with the result of the filter', () => {
+      const selector = '.my-klass'
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const expected = wrapper.find(selector)
+      const foundElement = new FoundElement(wrapper)
+
+      foundElement.filter(selector)
+
+      expect(foundElement.element).toEqual(expected)
+    })
+
+    it('returns itself', () => {
+      const wrapper = shallow(<div className="my-klass">My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+
+      const result = foundElement.filter('.my-klass')
+
+      expect(result).toBe(foundElement)
+    })
+  })
+
+  describe('#first', () => {
+    it('returns the first enzyme element', () => {
+      const element1 = <div className="bar">My Div</div>
+      const element2 = <div className="bar">My Other Div</div>
+      const elements = (
+        <div>
+          {element1}
+          {element2}
+        </div>
+      )
+      const wrapper = shallow(elements)
+      const foundElement = new FoundElement(wrapper)
+      const expected = wrapper.find('.bar').first()
+
+      const result = foundElement.find('.bar').first()
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('#last', () => {
+    it('returns the last enzyme element', () => {
+      const element1 = <div className="bar">My Div</div>
+      const element2 = <div className="bar">My Other Div</div>
+      const elements = (
+        <div>
+          {element1}
+          {element2}
+        </div>
+      )
+      const wrapper = shallow(elements)
+      const foundElement = new FoundElement(wrapper)
+      const expected = wrapper.find('.bar').last()
+
+      const result = foundElement.find('.bar').last()
+
+      expect(result).toEqual(expected)
+    })
+  })
+
+  describe('#length', () => {
+    it('returns the length', () => {
+      const wrapper = shallow(<div>My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+
+      const result = foundElement.length
+
+      expect(result).toBe(1)
+    })
+  })
+
+  describe('#map', () => {
+    describe('when wrap option is not defined', () => {
+      it('returns array of results wrapped in FoundElements', () => {
+        const myKlass = <div className="my-klass">My Div</div>
+        const otherKlass = <div className="other-klass">My Div</div>
+        const wrapper = shallow(<div>{myKlass}{otherKlass}</div>)
+        const foundElement = new FoundElement(wrapper)
+        const expected = new FoundElement(shallow(myKlass))
+
+        const result = foundElement.map(node => node.find('.my-klass'))
+
+        expect(result).toEqual([expected])
+      })
+    })
+
+    describe('when wrap option is true', () => {
+      it('returns array of results wrapped in FoundElements', () => {
+        const myKlass = <div className="my-klass">My Div</div>
+        const otherKlass = <div className="other-klass">My Div</div>
+        const wrapped = shallow(<div>{myKlass}{otherKlass}</div>)
+        const foundElement = new FoundElement(wrapped)
+        const expected = new FoundElement(shallow(myKlass))
+
+        const result = foundElement.map((node) => node.find('.my-klass'), true)
+
+        expect(result).toEqual([expected])
+      })
+    })
+
+    describe('when wrap option is false', () => {
+      it('returns array of results not wrapped in FoundElements', () => {
+        const myKlass = <div className="my-klass">My Div</div>
+        const otherKlass = <div className="other-klass">My Div</div>
+        const wrapper = shallow(<div>{myKlass}{otherKlass}</div>)
+        const foundElement = new FoundElement(wrapper)
+        const expected = shallow(myKlass)
+
+        const result = foundElement.map(node => node.find('.my-klass'), false)
+
+        expect(result).toEqual([expected])
+      })
+    })
+  })
+
+  describe('#prop', () => {
+    it('returns the specific prop', () => {
+      const wrapper = shallow(<div foo="bar">My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+
+      const result = foundElement.prop('foo')
+
+      expect(result).toBe('bar')
+    })
+  })
+
+  describe('#props', () => {
+    it('returns the props', () => {
+      const wrapper = shallow(<div foo="bar">My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+
+      const result = foundElement.props()
+
+      expect(result).toEqual({ children: 'My Div', foo: 'bar' })
+    })
+  })
+
+  describe('#text', () => {
+    it('returns the text', () => {
+      const wrapper = shallow(<div>My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+
+      const result = foundElement.text()
+
+      expect(result).toEqual('My Div')
+    })
+  })
+
+  describe('#type', () => {
+    it('returns the type from enzyme wrapper', () => {
+      const wrapper = shallow(<div>My Div</div>)
+      const foundElement = new FoundElement(wrapper)
+      const expected = wrapper.type()
+
+      const result = foundElement.type()
+
+      expect(result).toEqual(expected)
     })
   })
 })
